8fdf0debc83efae05550af5aae234f63c13dca29
kyranmccann <kyran.mccann@gmail.com>
2019-03-22 10:12:57 -0700

    finish two levels

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -3,32 +3,11 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-
-/**
- * Main
- */
-int main(int argc, char **argv)
-{
-  char *directory;
+int print_out(char *directory, int levels) {
   struct dirent *dir;
   struct stat buffer;
   //arbitrary reasonably-sized buffer
   char path[4096];
-
-  // Parse command line
-  if (argc == 2)
-  {
-    directory = argv[1];
-  }
-  else if (argc == 1)
-  {
-    directory = ".";
-  }
-  else
-  {
-    fprintf(stderr, "too many arguments");
-    exit(1);
-  }
 
   // Open directory
   DIR *d = opendir(directory);
@@ -46,17 +25,32 @@ int main(int argc, char **argv)
     if(stat(path, &buffer) < 0)
     {
       fprintf(stderr, "something wrong in stat");
-      exit(1)
+      exit(1);
     }
     else
     {
       if(buffer.st_mode&S_IFDIR)
       {
-        printf("<DIR> %s\n", dir->d_name);
+        if(levels > 1)
+        {
+          printf("<DIR> %s\n", dir->d_name);
+          print_out(path, levels-1);
+        }
+        else
+        {
+          printf("       <DIR> %s\n", dir->d_name);
+        }
       }
       else
       {
-        printf("%ld %s\n", buffer.st_size, dir->d_name);
+        if (levels > 1)
+        {
+          printf("       %ld %s\n", buffer.st_size, dir->d_name);
+        }
+        else
+        {
+          printf("       %ld %s\n", buffer.st_size, dir->d_name);
+        }
       }
     }
   }
@@ -64,4 +58,29 @@ int main(int argc, char **argv)
   // Close directory
   closedir(d);
   return 0;
+}
+
+/**
+ * Main
+ */
+int main(int argc, char **argv)
+{
+  char *directory;
+
+  // Parse command line
+  if (argc == 2)
+  {
+    directory = argv[1];
+  }
+  else if (argc == 1)
+  {
+    directory = ".";
+  }
+  else
+  {
+    fprintf(stderr, "too many arguments");
+    exit(1);
+  }
+
+  print_out(directory, 2);
 }
